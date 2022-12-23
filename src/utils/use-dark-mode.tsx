01b5c7ed48e03ae50cb3e81a4dcee6fb933b75c3
Bayathy <misosoup0321@icloud.com>
2022-12-23 12:02:42 +0900

    fix: remove stash code

--- src/utils/use-dark-mode.tsx
+++ src/utils/use-dark-mode.tsx
@@ -3,20 +3,12 @@ import { useEffect, useState } from "react";
 import type { Dispatch, SetStateAction } from "react";
 
 export const useDarkMode = (): [boolean, Dispatch<SetStateAction<boolean>>] => {
-  const [isDarkMode, toggleDarkMode] = useState<boolean>(() => {
-    return localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-      ? true
-      : false;
-  });
+  const [isDarkMode, toggleDarkMode] = useState<boolean>(false);
 
   useEffect(() => {
     if (isDarkMode) {
-      localStorage.theme = "dark";
       document.documentElement.classList.add("dark");
     } else {
-      localStorage.theme = "light";
       document.documentElement.classList.remove("dark");
     }
   }, [isDarkMode]);

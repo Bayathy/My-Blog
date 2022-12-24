71851974c1336ac7b28cd557ded03d624c398454
Bayathy <misosoup0321@icloud.com>
2022-12-24 16:55:26 +0900

    fix: re-style HomeLayout

--- src/component/layout/home-layout.tsx
+++ src/component/layout/home-layout.tsx
@@ -2,12 +2,19 @@ import type { FC, PropsWithChildren } from "react";
 import tw from "twin.macro";
 import { Footer } from "../footer";
 import { Header } from "../header";
+import { css } from "@emotion/react";
 
 export const HomeLayout: FC<PropsWithChildren> = ({ children }) => (
-  <>
+  <div
+    css={css`
+      display: grid;
+      grid-template-rows: auto 1fr auto;
+      grid-template-columns: 100%;
+      min-height: 100vh;
+    `}
+  >
     <Header />
-    <main css={tw`m-auto min-h-[90vh] max-w-3xl`}>{children}</main>
+    <main css={tw`m-auto max-w-3xl`}>{children}</main>
     <Footer />
-  </>
+  </div>
 );
-

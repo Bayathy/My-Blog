7ebdafec3dcaffb2f14464f5614afae61b69a096
Bayathy <misosoup0321@icloud.com>
2022-12-26 17:15:09 +0900

    feat: create get-article file

---
+++ src/utils/cms/get-article.ts
@@ -0,0 +1,12 @@
+import { client } from "./init";
+
+export type GetArticleRes = {
+  body: string;
+  createdAt: Date;
+  id: string;
+  tag: Array<string>;
+  title: string;
+};
+export const getArticle = async (id: string): Promise<GetArticleRes> => {
+  return await client.get({ endpoint: "blog", contentId: id });
+};

d915f3776dc85b5bf7773413668c231d0e359ca2
Bayathy <misosoup0321@icloud.com>
2022-12-28 00:22:46 +0900

    feat: create default seo

---
+++ next-seo.config.js
@@ -0,0 +1,9 @@
+export default {
+  title: "Bayathy Blog",
+  description: "BayathyのBlogです。色々書きます。",
+  twitter: {
+    handle: "@Kobath_dev",
+    site: "bayathy.com",
+    cardType: "summary_large_image",
+  },
+};

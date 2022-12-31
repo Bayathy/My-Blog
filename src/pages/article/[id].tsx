b0c7769e5a417d8eb4e99cbc12618c15190638cf
Bayathy <misosoup0321@icloud.com>
2022-12-31 18:50:24 +0900

    fix: fix aritcle card

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -56,18 +56,16 @@ const Article: NextPageWithLayout<StaticArticlePageProps> = ({ data }) => (
       }}
     />
     <article css={tw`dark:text-white`}>
-      <div css={tw`border-b-2 border-black pb-7 dark:border-white`}>
-        <ArticleCard
-          title={data.title}
-          created_at={new Date(data.createdAt)}
-          tag={data.tag}
-        />
-      </div>
+      <ArticleCard
+        title={data.title}
+        created_at={new Date(data.createdAt)}
+        tag={data.tag}
+      />
       <div
         css={[
           tw`[&>h1]:my-3 [&>h1]:text-3xl`,
           tw`[&>h2]:my-4 [&>h2]:border-l-8 [&>h2]:border-black [&>h2]:border-l-secondary-light [&>h2]:px-2 [&>h2]:text-2xl [&>h2]:dark:border-l-secondary-dark`,
-          tw`[&>h3]:text-xl`,
+          tw`[&>h3]:my-2 [&>h3]:border-b-2 [&>h3]:border-black [&>h3]:text-xl [&>h3]:dark:border-white`,
           tw`[&>l1]:list-inside`,
           tw`[&>img]:m-auto`,
           tw`[&>pre]:bg-black [&>pre]:text-white`,
